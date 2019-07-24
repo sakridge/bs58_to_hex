74e7c9578211f232c6281d6f24a8d8282123e06a
Stephen Akridge <sakridge@gmail.com>
2019-07-24 11:00:18 -0700

    bs58_to_hex program

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use bs58;
+use hex;
+use std::process::exit;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: bs58_to_hex <bs58>");
+        exit(1);
+    }
+    let bs58_str = &args[1];
+    let x = bs58::decode(bs58_str).into_vec().unwrap();;
+    println!("{}", hex::encode(x));
+}
